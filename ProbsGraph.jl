f976e0e06ed0e7c6e27135a97f4642edf36b9f2a
Paul Boone <narcissus.pong@gmail.com>
2020-02-17 14:48:02 -0500

    working plot of molecule trial insertion probabilities

--- ProbsGraph.jl
+++ ProbsGraph.jl
@@ -1,63 +1,40 @@
 using VegaLite
 using DataFrames
 using LinearAlgebra
+import Statistics
 
 function probs_graph(probs)
     num_rows = *(size(probs)...)
-    df = DataFrame(
-            num_molecules=[(i - 1) % size(probs,1) + 1 for i = 1:num_rows],
-            molecule_id=[(i - 1) ÷ size(probs,1) + 1 for i = 1:num_rows],
-            prob=log10.(replace(vec(probs), 0.0=>NaN))
-        )
 
-    println(df[:, :molecule_id])
     plots = @vlplot(
-                data=df,
-                x={"num_molecules", title="Molecule insert #"},
-                y={"prob", title="log(insert probability)"},
-                color="molecule_id:o",
-                width=1200,
-                height=1200
+                data=DataFrame(
+                        num_molecules=[(i - 1) % size(probs,1) + 1 for i = 1:num_rows],
+                        molecule_id=[(i - 1) ÷ size(probs,1) + 1 for i = 1:num_rows],
+                        prob=log10.(replace(vec(probs), 0.0=>NaN))
+                    ),
+                x={"num_molecules", title="Molecule insert #", type="quantitative"},
+                y={"prob", title="log(insert probability)", type="quantitative"},
+                opacity={value=1},
+                width=800,
+                height=800
             ) +
-            @vlplot(mark=({type=:line, point=true})) +
-            # @vlplot(mark={type=:text, dy=-7},
-            #         text={field=:molecule_id, type=:ordinal}
-            #         ) +
-            @vlplot(mark={type=:point, size=100, alpha=1.0},
+            @vlplot(mark={type=:line},
+                color={field="molecule_id", type="ordinal", legend=false,
+                    scale={scheme={name="blues", extent=[0.2,1.0], count=num_rows, minOpacity=1.0, maxOpacity=1.0}}},
+                    ) +
+            @vlplot(mark={type=:point, size=100},
                     data=DataFrame(molecule_id=1:39, y=log10.(diag(probs))),
                     x=:molecule_id,
                     y=:y,
-                    color="molecule_id:o",
-                    # fill="molecule_id:o",
-
+                    fill={field="molecule_id", type="ordinal", scale={scheme={name="blues", extent=[0.2,1.0], count=num_rows, minOpacity=1.0, maxOpacity=1.0}}},
+                    ) +
+            @vlplot(:line,
+                    data=DataFrame(num_molecules=1:39, median=[Statistics.median(log10.(probs[i,1:i])) for i=1:39]),
+                    x=:num_molecules,
+                    y=:median,
+                    color={value="black"},
+                    size={value=4},
                     )
 
-
-
-
-                    # text={field=:molecule_id, type=:ordinal},
-                    # text={field=:molecule_id, type=:nominal},
-                    # )
-            #         +
-            # @vlplot(mark=:line,
-            #
-            #         )
-            #
-            # plots = @vlplot(mark={:line, point=true, :text=true},
-            #                 enc={
-            #                     x={"num_molecules:o", title="Molecule insert #"},
-            #                     y={"prob:q", title="log(insert probability)"},
-            #                     color=:molecule_id
-            #                 },
-            #                 title="asdf",
-            #                 width=1200,
-            #                 height=1200
-            #
-            #                 # text={field=:molecule_id, type=:ordinal},
-            #                 # text={field=:molecule_id, type=:nominal},
-            #                 )
-
-
     plots |> display
-    # df |> plots |> display
 end
