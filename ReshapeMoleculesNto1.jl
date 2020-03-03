9d50d829c0f191553095546ae263a12b71cb69ff
Paul Boone <narcissus.pong@gmail.com>
2020-03-03 14:27:36 -0500

    attempt to break up N clusters of H2O into single H2Os

---
+++ ReshapeMoleculesNto1.jl
@@ -0,0 +1,30 @@
+using JLD2
+using PorousMaterials
+
+
+function load_molecules_from_checkpoint(checkpoint_path, molecule="TIP4P-1")
+
+    @load checkpoint_path checkpoint
+
+    h2o_singles = Molecule[]
+
+    for m in checkpoint["molecules"]
+        println("$(m.atoms.n_atoms) h2os in molecule")
+        for h2o_index in 1:m.atoms.n_atoms
+
+            atoms = Atoms([m.atoms.species[1]], m.atoms.xf[:, h2o_index:h2o_index])
+
+            # 3 charges per H2O
+            chargerange = 3*h2o_index - 2:h2o_index * 3
+            charges = Charges(3, [-1.04, 0.52, 0.52], m.charges.xf[:, chargerange])
+            com = atoms.xf[:, 1]
+            new_h2o = Molecule(Symbol(molecule), atoms, charges, com)
+
+            push!(h2o_singles, new_h2o)
+        end
+    end
+
+    println("resulting h2os: $(size(h2o_singles)) ")
+
+    return h2o_singles
+end
