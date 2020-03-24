3244d4435de84e78d44bdd04e45cc8d611893706
Paul Boone <narcissus.pong@gmail.com>
2020-03-24 19:52:23 -0400

    refactor into separate function so it can be called outside of using a checkpoint

--- ReshapeMoleculesNto1.jl
+++ ReshapeMoleculesNto1.jl
@@ -11,29 +11,31 @@ Takes all the adsorbed molecules (typically an H2O N-cluster) from a checkpoint 
 N-cluster into its individual H2O molecules (typically, a TIP4P-1).
 """
 function reshape_h2o_cluster_to_single_h2os_from_checkpoint(checkpoint, molecule="TIP4P-1")
-
-    h2o_singles = Molecule[]
-
     for m in checkpoint["molecules"]
         println("$(m.atoms.n_atoms) h2os in molecule")
-
-        # each TIP4P is ONE atom, the oxygen, + three charges, two where the hydrogens should be and
-        # one that is offset from the oxygen. So each TIP4P H2O has one atom and three charges.
-        for h2o_index in 1:m.atoms.n_atoms
-            atoms = Atoms([m.atoms.species[1]], m.atoms.xf[:, h2o_index:h2o_index])
-
-            # 3 charges per H2O
-            chargerange = 3*h2o_index - 2:h2o_index * 3
-            charges = Charges(3, [-1.04, 0.52, 0.52], m.charges.xf[:, chargerange])
-            com = atoms.xf[:, 1]
-            new_h2o = Molecule(Symbol(molecule), atoms, charges, com)
-
-            push!(h2o_singles, new_h2o)
-        end
+        h2o_singles = h2o_singles_from_n_cluster(m, molecule)
     end
 
     println("resulting h2os: $(size(h2o_singles)) ")
 
+    return h2o_singles
+end
+
+function h2o_singles_from_n_cluster(m, molecule)
+    # each TIP4P is ONE atom, the oxygen, + three charges, two where the hydrogens should be and
+    # one that is offset from the oxygen. So each TIP4P H2O has one atom and three charges.
+    h2o_singles = Molecule[]
+    for h2o_index in 1:m.atoms.n_atoms
+        atoms = Atoms([m.atoms.species[1]], m.atoms.xf[:, h2o_index:h2o_index])
+
+        # 3 charges per H2O
+        chargerange = 3*h2o_index - 2:h2o_index * 3
+        charges = Charges(3, [-1.04, 0.52, 0.52], m.charges.xf[:, chargerange])
+        com = atoms.xf[:, 1]
+        new_h2o = Molecule(Symbol(molecule), atoms, charges, com)
+
+        push!(h2o_singles, new_h2o)
+    end
     return h2o_singles
 end
 
