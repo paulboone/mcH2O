789b1aab94e1d011c9856f00bc9268c82cd744f5
Paul Boone <narcissus.pong@gmail.com>
2020-03-23 21:54:37 -0400

    plot settings for last egraph run–shouldn't be a commit; needs cleaning

--- egraph-plotsjl.jl
+++ egraph-plotsjl.jl
@@ -2,16 +2,16 @@ using CSV, DataFrames, Plots
 import Plots.PlotMeasures: px
 
 
-e_df_batch = CSV.File("h2o_4500_mc_n1000_batch/energy_log_H2O_tip4p_4500.0_n1000_batch.tsv") |> DataFrame
+e_df_batch = CSV.File("insert_h2o_dimer/h2o_dimer_4500_mc_n1000_batch/energy_log_H2O_tip4p_dimer_4500.0_n1000_batch.tsv") |> DataFrame
 e_df_batch.e_total = e_df_batch.gh_vdw .+ e_df_batch.gh_q .+ e_df_batch.gg_vdw .+ e_df_batch.gg_q
-e_df_baseline = CSV.File("h2o_4500_mc_n1000_baseline/energy_log_H2O_tip4p_4500.0_n1000_baseline.tsv") |> DataFrame
+e_df_baseline = CSV.File("insert_h2o_dimer/h2o_dimer_4500_mc_n1000_baseline/energy_log_H2O_tip4p_dimer_4500.0_n1000_baseline.tsv") |> DataFrame
 e_df_baseline.e_total = e_df_baseline.gh_vdw .+ e_df_baseline.gh_q .+ e_df_baseline.gg_vdw .+ e_df_baseline.gg_q
 
 function eplots(e_df, ebounds, nbounds; legend=false, y_formatter=:plain, noylabel=false, title="")
   ylabel = noylabel ? "" : "Energy [K] / 1000"
   leftmargin = noylabel ? 0px : 40px
   eplot = plot(e_df.cycle, e_df.gh_vdw / 1000, label="Guest-host VDW",
-            xlims = 0:404_000, xticks= 0:40_000:404_000, xlabel="MC Move Index",
+            xlims = 0:695000, xticks= 0:120_000:695000, xlabel="MC Move Index",
             ylabel=ylabel, ylims=ebounds,
             minorticks=10, minorgrid=true, x_formatter=:plain, legend=legend,
             y_formatter=y_formatter, top_margin=0px, left_margin=leftmargin,
@@ -25,7 +25,7 @@ function eplots(e_df, ebounds, nbounds; legend=false, y_formatter=:plain, noylab
 
   ylabel = noylabel ? "" : "# H2O"
   nplot = plot(e_df.cycle, e_df.num_adsorbates, label="",
-            xlims = 0:404_000, xticks= 0:40_000:404_000, xformatter=_->"",
+            xlims = 0:695000, xticks= 0:120_000:695000, xformatter=_->"",
             ylabel=ylabel, ylims=nbounds, bottom_margin=0px, y_minorticks=3,
             x_minorticks=10, minorgrid=true, y_formatter=y_formatter, title=title,
             left_margin=leftmargin)
@@ -37,7 +37,7 @@ ebounds = (minimum(describe(e_df_batch[:, 3:6])[:, :min]), maximum(describe(e_df
 nbounds = (0, maximum(e_df_batch[:, :num_adsorbates]))
 
 batche, batchn = eplots(e_df_batch, ebounds, nbounds, legend=false, title="Batch Method")
-baselinee, baselinen = eplots(e_df_baseline, ebounds, nbounds, legend=:bottomright,
+baselinee, baselinen = eplots(e_df_baseline, ebounds, nbounds, legend=:right,
     y_formatter=_->"", noylabel=true, title="Baseline Method")
 
 master_plot = plot(batchn, baselinen, batche, baselinee, size=(1200,800), dpi=300, layout=grid(2,2,heights=[0.25,0.75]))
