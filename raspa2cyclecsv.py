6770a12d8ccdcbd1b659e0064051f9e86f2c584c
Paul Boone <narcissus.pong@gmail.com>
2020-04-16 21:56:54 -0400

    make into command line script and catch EOF correctly

--- raspa2cyclecsv.py
+++ raspa2cyclecsv.py
@@ -1,45 +1,55 @@
+#!/usr/bin/env python3
 import csv
 import re
 import sys
 
-path = "2020-04-11/ZIF8-N2-1000/Output/System_0/output_ZIF-8_2.2.2_298.000000_1000.data"
+import click
 
-with open(path, 'r') as f:
-    csvout = csv.writer(sys.stdout)
-    # go to start of simulation
-    while next(f).strip() != "Starting simulation": pass
+@click.command()
+@click.argument('path', type=click.Path())
+def raspa2cyclescsv(path):
+    with open(path, 'r') as f:
+        csvout = csv.writer(sys.stdout)
+        # go to start of simulation
+        while next(f).strip() != "Starting simulation": pass
 
-    cycle_re = r"(\[Init\] )*Current cycle: (\d+) out of \d+"
-    adsorbates_re = r"Number of Adsorbates: +(\d+) \((\d+) integer, (\d+) fractional, (\d+) reaction\)"
+        cycle_re = r"(\[Init\] )*Current cycle: (\d+) out of \d+"
+        adsorbates_re = r"Number of Adsorbates: +(\d+) \((\d+) integer, (\d+) fractional, (\d+) reaction\)"
 
-    on_init_cycles = True
-    cycle_diff = 0
-    cycles_1_2 = []
-    cycle_index = 0
+        on_init_cycles = True
+        cycle_diff = 0
+        cycles_1_2 = []
+        cycle_index = 0
 
-    while True:
-        # find next cycle
-        while (match := re.match(cycle_re, next(f).strip())) == None: pass
+        try:
+            while True:
+                # find next cycle
+                while (match := re.match(cycle_re, next(f).strip())) == None: pass
 
-        cycle_kind = match.groups()[0].strip()
-        cycle_num = int(match.groups()[1])
+                cycle_kind = match.groups()[0].strip() if match.groups()[0] is not None else ""
+                cycle_num = int(match.groups()[1])
 
-        # warning: automatically recognizing the cycles frequency requires at least TWO outputs
-        if cycle_diff == 0:
-            cycles_1_2.append(cycle_num)
-            if len(cycles_1_2) == 2:
-                cycle_diff = cycles_1_2[1] - cycles_1_2[0]
+                # warning: automatically recognizing the cycles frequency requires at least TWO outputs
+                if cycle_diff == 0:
+                    cycles_1_2.append(cycle_num)
+                    if len(cycles_1_2) == 2:
+                        cycle_diff = cycles_1_2[1] - cycles_1_2[0]
 
-        if cycle_diff > 0:
-            total_cycles = cycle_index * cycle_diff
-        else:
-            total_cycles = cycle_num
+                if cycle_diff > 0:
+                    total_cycles = cycle_index * cycle_diff
+                else:
+                    total_cycles = cycle_num
 
-        # find next # adsorbants
-        while (match := re.match(adsorbates_re, next(f).strip())) == None: pass
-        num_adsorbates = int(match.groups()[0])
+                # find next # adsorbants
+                while (match := re.match(adsorbates_re, next(f).strip())) == None: pass
+                num_adsorbates = int(match.groups()[0])
 
-        # output csv line
-        csvout.writerow([total_cycles, cycle_num, cycle_kind, num_adsorbates])
+                # output csv line
+                csvout.writerow([total_cycles, cycle_num, cycle_kind, num_adsorbates])
 
-        cycle_index += 1
+                cycle_index += 1
+        except StopIteration:
+            pass
+
+if __name__ == '__main__':
+    raspa2cyclescsv()
