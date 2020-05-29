787a4fc04f7ff794a6aff36d827bc022f20e9634
Paul Boone <narcissus.pong@gmail.com>
2020-05-29 12:17:21 -0400

    plot cycles now has a switch between raspa and pm

--- python/h2ohelp/plot_cycles_csv.py
+++ python/h2ohelp/plot_cycles_csv.py
@@ -6,14 +6,22 @@ import pandas as pd
 @click.command()
 @click.argument('input_file', type=click.File())
 @click.argument('output_path', type=click.Path())
-def plot_cycles_csv(input_file, output_path):
-    ads_df = pd.read_csv(input_file, names=["total_cycles", "cycles", "cycle_type", "num_adsorbants"])
+@click.option('--pm/--raspa', default=False)
+def plot_cycles_csv(input_file, output_path, pm=False):
+
+    if pm:
+        ads_df = pd.read_csv(input_file, sep="\t")
+        ads_df["total_cycles"] = ads_df.cycle
+    else:
+        ads_df = pd.read_csv(input_file, names=["total_cycles", "cycles", "cycle_type", "num_adsorbants"])
+        ads_df.rename(columns={'num_adsorbants': 'num_adsorbates',}, inplace=True)
 
     fig = plt.figure(figsize=(7,7))
     ax = fig.add_subplot(1, 1, 1)
-    ax.plot(ads_df.total_cycles, ads_df.num_adsorbants, zorder=2)
+    ax.plot(ads_df.total_cycles, ads_df.num_adsorbates, zorder=2)
     ax.set_xlabel('Total Cycles')
-    ax.set_ylabel('# Adsorbants')
+    ax.set_ylabel('# Adsorbates')
+    ax.set_ylim(bottom=0)
     ax.grid(linestyle='-', color='0.7', zorder=0)
     fig.savefig(output_path, dpi=300)
 
