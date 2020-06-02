a2716cd4c36a8b509cd928cc443c438c69979c6a
Paul Boone <narcissus.pong@gmail.com>
2020-06-02 17:23:37 -0400

    functions for lowest energy trial insertion, collecting a dataframe from all outputs, and plotting

--- BestClusterInserts.jl
+++ BestClusterInserts.jl
@@ -1,37 +1,43 @@
+using DataFrames
+
 include("GCMCTrials.jl")
 
-rep = 1
-for cluster_size = 2:21
-    molecule_name = "TIP4P-$(cluster_size)"
-    df = trial_insertions_data(molecule_name, 4500, 10, file_suffix="$rep")
+function run_trials(num_trials=10; rep=1)
+
+    for cluster_size = 2:21
+        molecule_name = "TIP4P-$(cluster_size)"
+        df = trial_insertions_data(molecule_name, 4500, num_trials, file_suffix="$rep")
+    end
 end
 
 
+function read_trial_results(path; num_trials=10000)
+    df = DataFrame(:cluster_size => zeros(Int, 100),
+                   :energy => zeros(100),
+                   :rep => zeros(Int, 100))
 
+    i = 1
+    for cluster_size = 2:21
+        for rep = 1:5
+            molecule_name = "TIP4P-$(cluster_size)"
+            checkpoint_file = "molchk-$(molecule_name)_$(num_trials)_$rep.jld2"
+            @load "$(path)$(checkpoint_file)" checkpoint
 
+            df.cluster_size[i] = cluster_size
+            df.energy[i] = checkpoint["best_energy"]
+            df.rep[i] = rep
+            i += 1
+        end
+    end
+    return df
+end
 
-#
-# include("ReshapeMoleculesNto1.jl")
-#
-# checkpoint_file = "$(chk_prefix).jld2"
-# @load checkpoint_file checkpoint
-# ## split molecules
-# molecules = reshape_h2o_cluster_to_single_h2os_from_checkpoint(checkpoint)
-#
-#
-#
-# args = Dict(
-#     :molecules => molecules,
-#     :batch_move_type => 1,
-#     :n_burn_cycles => 0,
-#     :n_sample_cycles => 800,
-#     :n_subcycles => 1000,
-#     :write_adsorbate_snapshots => false,
-#     :snapshot_frequency => 50,
-#     :molecule_multiplier => 1,
-#     :write_checkpoints => true,
-#     :checkpoint_frequency => 50,
-#     :n_cluster_intraenergy => 0.0
-# )
-# #
-# # include("../../n_mc2.jl")
+function plot_energy_vs_cluster_size(df)
+    p = plot(df.cluster_size, df.energy, seriestype=:scatter, xlabel="H2O Cluster Size", ylabel="Energy [K]",
+            dpi=300, size=(600, 600), legend=false)
+    savefig(p, "energy-vs-clustersizeN.png")
+end
+
+function best_trial(df)
+    return sort(df, (:cluster_size, :energy))[1:5:end,:]
+end
